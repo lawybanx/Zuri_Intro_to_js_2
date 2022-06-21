e696b842b6d6f5e2244dcea310150e40d710cc7b
Awwal Bankole <awwalbankole@gmail.com>
2022-06-21 19:39:08 +0100

    Completed task

---
+++ script.js
@@ -0,0 +1,9 @@
+const personalInfo = {
+  name: 'Bankole Muhammad-awwal',
+  height: '5ft 10 inches',
+  country: 'Nigeria',
+};
+
+alert(
+  `My name is ${personalInfo.name} and I am ${personalInfo.height} tall. I am from ${personalInfo.country}.`
+);
